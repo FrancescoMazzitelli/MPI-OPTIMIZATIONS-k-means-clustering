1795b0578f0a6fc3f6af57a9f3376bc094fc10cd
FrancescoMazzitelli <pannemar2@gmail.com>
2023-03-24 16:53:45 +0100

    Primo push

---
+++ random_points.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+double* create_rand_nums(const int num_elements) {
+  double *rand_nums = (double *)malloc(sizeof(double) * num_elements);
+  assert(rand_nums != NULL);
+  for (int i = 0; i < num_elements; i++) {
+    rand_nums[i] = (rand() / (float)RAND_MAX);
+  }
+  return rand_nums;
+}
+
+int main(int argc, char* argv){
+  int num_clusters = 15;
+  int tot_nums = 2592000;
+  double* random_nums = create_rand_nums(tot_nums);
+  FILE* output=fopen("input.txt","w");
+  fprintf(output, "%d\n", num_clusters);
+  fprintf(output, "%d\n", tot_nums/2);
+  for(int i=0; i<tot_nums; i=i+2){
+    fprintf(output,"%lf,",random_nums[i]);
+    fprintf(output,"%lf\n", random_nums[i+1]);
+  }
+}
